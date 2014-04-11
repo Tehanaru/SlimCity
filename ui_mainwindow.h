46398a57910e0e3e5aba00c4707b42704b896628
Tehanaru <drascus@gmail.com>
2014-04-11 11:04:06 -0700

    Signed-off-by: Tehanaru <drascus@gmail.com>

---
+++ ui_mainwindow.h
@@ -0,0 +1,137 @@
+/********************************************************************************
+** Form generated from reading UI file 'mainwindow.ui'
+**
+** Created by: Qt User Interface Compiler version 5.2.0
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_MAINWINDOW_H
+#define UI_MAINWINDOW_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QAction>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QButtonGroup>
+#include <QtWidgets/QFrame>
+#include <QtWidgets/QHeaderView>
+#include <QtWidgets/QMainWindow>
+#include <QtWidgets/QMenu>
+#include <QtWidgets/QMenuBar>
+#include <QtWidgets/QStatusBar>
+#include <QtWidgets/QToolBar>
+#include <QtWidgets/QWidget>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_MainWindow
+{
+public:
+    QAction *actionNew;
+    QAction *actionOpen;
+    QAction *actionSave;
+    QAction *actionQuit;
+    QAction *actionOptions;
+    QAction *actionHelp;
+    QAction *actionAbout;
+    QWidget *centralWidget;
+    QFrame *CityView;
+    QMenuBar *menuBar;
+    QMenu *menuSlim_City_Bare_Bones_Alpha;
+    QMenu *menuGame;
+    QMenu *menuHelp;
+    QToolBar *mainToolBar;
+    QStatusBar *statusBar;
+
+    void setupUi(QMainWindow *MainWindow)
+    {
+        if (MainWindow->objectName().isEmpty())
+            MainWindow->setObjectName(QStringLiteral("MainWindow"));
+        MainWindow->resize(800, 600);
+        QSizePolicy sizePolicy(QSizePolicy::Fixed, QSizePolicy::Fixed);
+        sizePolicy.setHorizontalStretch(0);
+        sizePolicy.setVerticalStretch(0);
+        sizePolicy.setHeightForWidth(MainWindow->sizePolicy().hasHeightForWidth());
+        MainWindow->setSizePolicy(sizePolicy);
+        MainWindow->setMinimumSize(QSize(800, 600));
+        MainWindow->setMaximumSize(QSize(800, 600));
+        actionNew = new QAction(MainWindow);
+        actionNew->setObjectName(QStringLiteral("actionNew"));
+        actionOpen = new QAction(MainWindow);
+        actionOpen->setObjectName(QStringLiteral("actionOpen"));
+        actionSave = new QAction(MainWindow);
+        actionSave->setObjectName(QStringLiteral("actionSave"));
+        actionQuit = new QAction(MainWindow);
+        actionQuit->setObjectName(QStringLiteral("actionQuit"));
+        actionOptions = new QAction(MainWindow);
+        actionOptions->setObjectName(QStringLiteral("actionOptions"));
+        actionHelp = new QAction(MainWindow);
+        actionHelp->setObjectName(QStringLiteral("actionHelp"));
+        actionAbout = new QAction(MainWindow);
+        actionAbout->setObjectName(QStringLiteral("actionAbout"));
+        centralWidget = new QWidget(MainWindow);
+        centralWidget->setObjectName(QStringLiteral("centralWidget"));
+        CityView = new QFrame(centralWidget);
+        CityView->setObjectName(QStringLiteral("CityView"));
+        CityView->setGeometry(QRect(90, 0, 700, 500));
+        CityView->setMinimumSize(QSize(700, 500));
+        CityView->setFrameShape(QFrame::StyledPanel);
+        CityView->setFrameShadow(QFrame::Raised);
+        MainWindow->setCentralWidget(centralWidget);
+        menuBar = new QMenuBar(MainWindow);
+        menuBar->setObjectName(QStringLiteral("menuBar"));
+        menuBar->setGeometry(QRect(0, 0, 800, 18));
+        menuSlim_City_Bare_Bones_Alpha = new QMenu(menuBar);
+        menuSlim_City_Bare_Bones_Alpha->setObjectName(QStringLiteral("menuSlim_City_Bare_Bones_Alpha"));
+        menuGame = new QMenu(menuBar);
+        menuGame->setObjectName(QStringLiteral("menuGame"));
+        menuHelp = new QMenu(menuBar);
+        menuHelp->setObjectName(QStringLiteral("menuHelp"));
+        MainWindow->setMenuBar(menuBar);
+        mainToolBar = new QToolBar(MainWindow);
+        mainToolBar->setObjectName(QStringLiteral("mainToolBar"));
+        MainWindow->addToolBar(Qt::TopToolBarArea, mainToolBar);
+        statusBar = new QStatusBar(MainWindow);
+        statusBar->setObjectName(QStringLiteral("statusBar"));
+        MainWindow->setStatusBar(statusBar);
+
+        menuBar->addAction(menuSlim_City_Bare_Bones_Alpha->menuAction());
+        menuBar->addAction(menuGame->menuAction());
+        menuBar->addAction(menuHelp->menuAction());
+        menuSlim_City_Bare_Bones_Alpha->addAction(actionNew);
+        menuSlim_City_Bare_Bones_Alpha->addAction(actionOpen);
+        menuSlim_City_Bare_Bones_Alpha->addAction(actionSave);
+        menuSlim_City_Bare_Bones_Alpha->addAction(actionQuit);
+        menuGame->addAction(actionOptions);
+        menuHelp->addAction(actionHelp);
+        menuHelp->addAction(actionAbout);
+
+        retranslateUi(MainWindow);
+
+        QMetaObject::connectSlotsByName(MainWindow);
+    } // setupUi
+
+    void retranslateUi(QMainWindow *MainWindow)
+    {
+        MainWindow->setWindowTitle(QApplication::translate("MainWindow", "MainWindow", 0));
+        actionNew->setText(QApplication::translate("MainWindow", "New", 0));
+        actionOpen->setText(QApplication::translate("MainWindow", "Open", 0));
+        actionSave->setText(QApplication::translate("MainWindow", "Save", 0));
+        actionQuit->setText(QApplication::translate("MainWindow", "Quit", 0));
+        actionOptions->setText(QApplication::translate("MainWindow", "Options", 0));
+        actionHelp->setText(QApplication::translate("MainWindow", "Help", 0));
+        actionAbout->setText(QApplication::translate("MainWindow", "About", 0));
+        menuSlim_City_Bare_Bones_Alpha->setTitle(QApplication::translate("MainWindow", "File", 0));
+        menuGame->setTitle(QApplication::translate("MainWindow", "Game", 0));
+        menuHelp->setTitle(QApplication::translate("MainWindow", "Help", 0));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class MainWindow: public Ui_MainWindow {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_MAINWINDOW_H
